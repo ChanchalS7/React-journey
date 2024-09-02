8eaa2eab5b54ca851262f9ba72ea71fd7d1ca9ae
ChanchalS7 <chsverma7@gmail.com>
2024-09-02 19:08:04 +0530

    added state variable in class component

--- day8/src/components/User.jsx
+++ day8/src/components/User.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 
 const User = ({ name }) => {
-	const [count] = useState(0)
+	// const [count] = useState(0)
+	//How to update state in functional component
+	const [count, setCount] = useState(0)
+	const [count2] = useState(2)
 	return (
 		<div className='user-card'>
 			{/* <h1>Chanchal V</h1> */}
-			<h3>Count={count}</h3>
+			<h2>Count={count}</h2>
+			<h2>Count2={count2}</h2>
 			<h1>{name}</h1>
 			<h2>Location:Indore</h2>
 			<h3>Linkedin:linkedin.com/in/chanchals7</h3>
--- day8/src/components/UserClass.jsx
+++ day8/src/components/UserClass.jsx
@@ -7,16 +7,26 @@ class UserCalss extends React.Component {
 		super(props);
 		// console.log("class-props:", props)
 		this.state = {
-			count: 0
+			count: 0,
+			// count2: 2
 		}
 	}
 	render() {
 		const { name } = this.props;
+		const { count, count2 } = this.state;
 
 		return (
 
 			<div className='user-card'>
-				<h3></h3>
+				<h2>Count = {this.state.count}</h2>
+				{/* <h2>Count2={count2}</h2> */}
+				<button onClick={() => {
+
+					//NEVER UPDATE STATE VARIABLES DIRECTLY
+					this.setState({
+						count: this.state.count + 1,
+					})
+				}}>Count Increase</button>
 				<h1>{name}</h1>
 				<h2>Location:Indore</h2>
 				<h3>Linkedin:linkedin.com/in/chanchals7</h3>
@@ -29,3 +39,4 @@ class UserCalss extends React.Component {
 }
 
 export default UserCalss
+//Never udpate states variable directly in class-component
