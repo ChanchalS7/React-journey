76634442a7c6b34923a6df43f0277b35bd08b64e
ChanchalS7 <chsverma7@gmail.com>
2024-09-20 11:15:30 +0530

    added day11

--- day11/src/components/RestaurantMenu.jsx
+++ day11/src/components/RestaurantMenu.jsx
@@ -1,7 +1,9 @@
+
 import { useEffect, useState } from 'react'
 import Shimmer from './Shimmer'
 import { useParams } from 'react-router-dom'
 import useRestaurantMenu from './../Hooks/useRestaurantMenus';
+import RestaurantCategory from './RestaurantCategory';
 const RestaurantMenu = () => {
 	const { resId } = useParams();
 
@@ -9,19 +11,19 @@ const RestaurantMenu = () => {
 
 	if (resInfo === null) return <Shimmer />
 	const { name, costForTwoMessage, cuisines } = resInfo?.cards[2]?.card?.card?.info;
-	const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+	const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card ||[]
+	console.log("menu", resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
+		const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=> c?.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
+
 	return (
 
-		<div className='menu'>
-			<h1>{name}</h1>
-			<p>{cuisines.join(", ")} - {costForTwoMessage}</p>
-			<h2>Menu</h2>
-			<ul style={{ marginLeft: "50px" }}>
-				{itemCards.map((item) => (
-					<li key={item.card.info.id}>•{item.card.info.name} - Rs.:{item.card.info.price / 100 || item.card.info.defaultPrice / 100}</li>
-				))}
-
-			</ul>
+		<div className='text-center'>
+			<h className='font-bold my-6 text-2xl'>{name}</h>
+			<p className='font-bold text-lg'>{cuisines.join(", ")} - {costForTwoMessage}</p>
+			{/* Categories accordian */}
+			{
+				categories.map((category) => <RestaurantCategory key={category.card.card.title } data={category?.card?.card} />)
+			}
 		</div>
 	)
 }
--- readme.md
+++ readme.md
@@ -1,12 +1,1 @@
-
-Two types of Export/Import
-
--Default Export/Import
-
-export default Component
-import Component from "path";
-
--Named Export/Import;
-
-export const Component
-import {Component} from "path";
+React Refreshing
